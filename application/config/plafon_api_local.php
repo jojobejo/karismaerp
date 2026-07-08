b75778cd37376f1cc9abb2e11f98ee0fba57035e
Anang Prayoga <Prayoga@karisma>
2026-07-08 12:24:22 +0700

    update database

---
+++ application/config/plafon_api_local.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['plafon_api_key'] = '8EON+uENl4TmtTmchwuzkZeJuz0tAANMNN1Wnj2RniM=';
+$config['plafon_api_base_url'] = 'https://plafon.karismaerp.com';
